b6a78dee15f619e821f95aab23b160590e8f07ee
Tallyson Freitas <tallyson.eu@outlook.com>
2023-10-30 14:28:36 -0300

    animacoes dos containers

--- src/containers/Formulario/style.ts
+++ src/containers/Formulario/style.ts
@@ -1,12 +1,17 @@
 import styled from 'styled-components'
+import { PositionDivType } from '../RegisterDiv/style'
 
-export const ContainerFormulario = styled.div`
+export const ContainerFormulario = styled.div<PositionDivType>`
   width: 50%;
   height: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  position: absolute;
+  transform: ${(props) => props.position === true && 'translateX(100%)'};
+  top: 0;
+  transition: all ease 1s;
 `
 
 export const SLista = styled.ul`
--- src/containers/RegisterDiv/index.tsx
+++ src/containers/RegisterDiv/index.tsx
@@ -5,15 +5,29 @@ import { SRegisterDiv } from './style'
 
 const Register = ({ altera, initial }: { altera: any; initial: boolean }) => {
   return (
-    <SRegisterDiv position={initial}>
-      <STitulo tipo="secundario">Hello, Friend!</STitulo>
-      <SText tipo="secundario">
-        Register with your personal details to use all of site features
-      </SText>
-      <SSingButton tipo="secundario" onClick={altera}>
-        SING UP
-      </SSingButton>
-    </SRegisterDiv>
+    <>
+      {initial === true ? (
+        <SRegisterDiv position={initial}>
+          <STitulo tipo="secundario">Wellcome Back!</STitulo>
+          <SText tipo="secundario">
+            Enter your personal details to use all of site features
+          </SText>
+          <SSingButton tipo="secundario" onClick={altera}>
+            SING IN
+          </SSingButton>
+        </SRegisterDiv>
+      ) : (
+        <SRegisterDiv position={initial}>
+          <STitulo tipo="secundario">Hello, Friend!</STitulo>
+          <SText tipo="secundario">
+            Register with your personal details to use all of site features
+          </SText>
+          <SSingButton tipo="secundario" onClick={altera}>
+            SING UP
+          </SSingButton>
+        </SRegisterDiv>
+      )}
+    </>
   )
 }
 
