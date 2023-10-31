ee1ef341c3b00f0d1b6eae085988f766b3d9c7ab
Tallyson Freitas <tallyson.eu@outlook.com>
2023-10-31 10:42:29 -0300

    Introduzindo responsividade a diferentes aparelhos

--- src/components/Titulo/style.ts
+++ src/components/Titulo/style.ts
@@ -7,4 +7,7 @@ export type TituloType = {
 export const STitulo = styled.h1<TituloType>`
   font-size: 40px;
   color: ${(props) => (props.tipo == 'primario' ? '#000' : '#fff')};
+  @media (max-width: 700px) {
+    font-size: 32px;
+  }
 `
